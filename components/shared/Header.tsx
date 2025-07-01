00a6661bfa8c771244188efcf0f540899c88a693
Akash Purohit <akashpurohit1235@gmail.com>
2025-07-01 19:48:23 +0530

    sign-up page

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,28 +3,37 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
-        <div className="wrapper flex items-center justify-center">
-            <Link href="/" className='w-36'>
-            <Image src="/assets/images/logo.svg" width={128} height={38} alt='evently-logo'/>
-            </Link>
+      <div className="wrapper flex items-center justify-center">
 
-            <div className='flex w-32 justify-end'>
+        <Link href="/" className='w-36'>
+          <Image src="/assets/images/logo.svg" width={128} height={38} alt='evently-logo' />
+        </Link>
 
-              <SignedIn>
-                <UserButton afterSignOutUrl="/"/>
-              </SignedIn>
-              <SignedOut>
-                <Button className='rounded-full' asChild size="lg">
-                  <Link href="/sign-in">Login
-                  </Link>
-                </Button>
-              </SignedOut>  
-            </div>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems/>
+          </nav>
+        </SignedIn>
+
+        <div className='flex w-32 justify-end'>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav/>
+          </SignedIn>
+          <SignedOut>
+            <Button className='rounded-full' asChild size="lg">
+              <Link href="/sign-in">Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
+      </div>
 
     </header>
   )
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+import { Separator } from '../ui/separator';
+
+const MobileNav = () => {
+
+    return (
+        <div className='md:hidden'>
+            <Sheet>
+                <SheetTrigger className='align-middle'>
+                    <Image src="/assets/icons/menu.svg" alt='menu' width={24} height={24} className='cursor-pointer'/>
+                </SheetTrigger>
+                <SheetContent className='flex flex-col gap-6 md:hidden bg-white'>
+                    <Image
+                    src="/assets/images/logo.svg"
+                    alt='logo'
+                    width={128}
+                    height={38}
+                    />
+                    <Separator className='border border-gray-50'/>
+                </SheetContent>
+            </Sheet>
+        </div>
+    )
+}
+
+export default MobileNav

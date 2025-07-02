93a9b15e073206960d73a54ad7e753a50459be56
Akash Purohit <akashpurohit1235@gmail.com>
2025-07-02 23:59:29 +0530

    footer page

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer className='border-t'>
+      <div>
+        <Link href='/'>
+        <Image
+        src="/assets/images/logo.svg"
+        alt='logo'
+        width={128}
+        height={38}
+        />
+        </Link>
+      </div>
+
+    </footer>
   )
 }
 

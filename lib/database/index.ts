9ae9ece1dbf3464dcbf15c1d188a285edc7d9db4
Akash Purohit <akashpurohit1235@gmail.com>
2025-07-07 19:52:54 +0530

    added logs again

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,14 +5,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  console.log("first line of db function");
   if (cached.conn) return cached.conn;
+  console.log("second line");
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  console.log("reached to the connecttodatabase");
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'evently',
     bufferCommands: false,
   })
+  console.log("passed the dbname attribute");
 
   cached.conn = await cached.promise;
 

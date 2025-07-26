dc36319165d11473bc7c7c455c3f80910e6b0676
Akash Purohit <akashpurohit1235@gmail.com>
2025-07-27 00:30:46 +0530

    event page4

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,18 +3,30 @@ import Image from "next/image";
 // import { CheckoutButton } from "@/components/shared/checkout-button";
 // import { Collection } from "@/components/shared/collection";
 import {
-  getEventById,
-  getRelatedEventsByCategory,
+    getEventById,
+    getRelatedEventsByCategory,
 } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import type { SearchParamProps } from "@/types";
 
-const EventDetails = () => {
-  return (
-    <div>
-      page 
-    </div>
-  )
+const EventDetails = async ({
+    params: { id },
+    searchParams,
+}: SearchParamProps) => {
+    const event = await getEventById(id);
+
+    const relatedEvents = await getRelatedEventsByCategory({
+        categoryId: event.category._id,
+        eventId: event._id,
+        page: searchParams.page as string,
+    });
+
+
+    return (
+        <div>
+            page
+        </div>
+    )
 }
 
 export default EventDetails
